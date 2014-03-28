20629b6ec22f2eb8866f042615b80cd975ee3157
Alejandro Ciniglio <mail@alejandrociniglio.com>
2014-03-28 13:49:53 -0400

    Some tweaks

--- init.el
+++ init.el
@@ -17,10 +17,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
- '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "white")))))
+ '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
+ '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify)))
