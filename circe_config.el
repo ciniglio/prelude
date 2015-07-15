d3d6ce045ea338e3f6fd69236a213fcd06551f60
Alejandro Ciniglio <mail@alejandrociniglio.com>
2015-07-15 17:59:38 -0400

    ERC Additions

--- circe_config.el
+++ circe_config.el
@@ -1,6 +1,4 @@
 (setq my-credentials-file "~/.emacs.d/.private.el")
-(setq circe-reduce-lurker-spam t)
-(enable-circe-color-nicks)
 
 (unless (file-exists-p "~/.emacs.d/.private.el")
   (display-warning 'init "Missing credentials file .private.el"))
@@ -10,10 +8,30 @@
     (insert-file-contents-literally my-credentials-file)
     (plist-get (read (buffer-string)) :nickserv-password)))
 
-(setq circe-network-options
-      '(("Freenode"
-         :nick "_alejandro"
-         :channels ("#emacs" "#emacs-circe"
-                    "#clojure" "#ansible"
-                    "#reactjs")
-         :nickserv-password my-nickserv-password)))
+(defun erc-settings ()
+  "Settings of `erc'."
+  (require 'erc-highlight-nicknames)
+
+  (add-to-list 'erc-modules 'highlight-nicknames)
+  (erc-update-modules)
+
+  (setq erc-nick-uniquifier "0"))
+
+(eval-after-load "erc"
+  `(erc-settings))
+
+(require 'erc-hl-nicks)
+
+;; (require 'erc-highlight-nicknames)
+;; (add-to-list 'erc-modules 'highlight-nicknames)
+;; (erc-update-modules)
+
+
+
+;; (setq circe-network-options
+;;       '(("Freenode"
+;;          :nick "_alejandro"
+;;          :channels ("#emacs" "#emacs-circe"
+;;                     "#clojure" "#ansible"
+;;                     "#reactjs")
+;;          :nickserv-password my-nickserv-password)))
