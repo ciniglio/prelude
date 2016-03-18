e4f1c3ee8df3cbe24be78a072c8606997bd60f6e
Alejandro Ciniglio <ciniglio@google.com>
2016-03-18 08:16:38 -0500

    Attempt to fix terminal colors

--- init.el
+++ init.el
@@ -244,8 +244,41 @@
           (setq sane-term-shell-command "/bin/zsh")
           (add-hook 'term-mode-hook
                     (lambda()
-                      (setq yas-dont-activate t
-                            term-buffer-maximum-size 10000))))
+                      (progn
+                        (setq yas-dont-activate t
+                              term-buffer-maximum-size 10000)
+                        (defface term-color-black 
+                          '((t (:foreground "#3f3f3f" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-red
+                          '((t (:foreground "#cc9393" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-green
+                          '((t (:foreground "#7f9f7f" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-yellow
+                          '((t (:foreground "#f0dfaf" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-blue 
+                          '((t (:foreground "#6d85ba" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-magenta 
+                          '((t (:foreground "#dc8cc3" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-cyan
+                          '((t (:foreground "#93e0e3" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        (defface term-color-white
+                          '((t (:foreground "#dcdccc" :background "#272822"))) 
+                          "Unhelpful docstring.")
+                        '(term-default-fg-color ((t (:inherit term-color-white))))
+                        '(term-default-bg-color ((t (:inherit term-color-black))))
+                        
+                        ;; ansi-term colors
+                        (setq ansi-term-color-vector
+                              [term term-color-black term-color-red term-color-green term-color-yellow 
+                                    term-color-blue term-color-magenta term-color-cyan term-color-white]))
+)))
   :config (progn
             (defun my/sane-term (arg)
               (interactive "P")
